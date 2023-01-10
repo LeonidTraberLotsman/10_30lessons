0c00890a423f70d1b703075962286389b4abb397
lotsman_board <leonidt@lotsman.org>
2023-01-10 10:38:13 +0500

    main menu

--- test6/Assets/scripts/LevelManager.cs
+++ test6/Assets/scripts/LevelManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class LevelManager : MonoBehaviour
     public void Restart()
     {
         Debug.Log("restarted");
+        SceneManager.LoadScene(1);
     }
     // Update is called once per frame
     void Update()
